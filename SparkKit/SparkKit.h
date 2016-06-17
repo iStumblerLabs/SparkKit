1be74f39b4b4bff9eb55c8d3abdf9e01c90af9e7
Alf Watt <alf@istumbler.net>
2016-06-16 18:38:14 -0700

    Initial Commit

---
+++ SparkKit/SparkKit.h
@@ -0,0 +1,19 @@
+//
+//  SparkKit.h
+//  SparkKit
+//
+//  Created by Alf Watt on 6/16/16.
+//  Copyright © 2016 iStumbler Labs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SparkKit.
+FOUNDATION_EXPORT double SparkKitVersionNumber;
+
+//! Project version string for SparkKit.
+FOUNDATION_EXPORT const unsigned char SparkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SparkKit/PublicHeader.h>
+
+
